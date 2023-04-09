4f6d7dd8e3b4e72a59e89cea5c0dc6f109d69ae9
Juul Christiaens <juulchristiaens@MacBook-Pro-van-Juul.local>
2023-04-09 19:49:32 +0200

    added a css file and started working on index.js to setup the rendering process

--- my-first-app/src/index.js
+++ my-first-app/src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App"
+
+const rootElement = document.getElementById("root")  //selects element "root" from the html file
+const root = createRoot(rootElement)
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <StrictMode> 
+    <App />   
+  </StrictMode>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// StrictMode is a tool that highlights potential issues in a programme. We encapsulate our code as a component. It will not render anything to the DOM.
+// This is the code that's going to be rendered to the DOM.
