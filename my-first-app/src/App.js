b677b4f380a0764cf73a4e93ce60b0f46dd784ec
Juul Christiaens <juulchristiaens@MacBook-Pro-van-Juul.local>
2023-04-09 21:00:36 +0200

    filled app.js to make a functional carousel

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"; //handy link to understand useState => https://www.w3schools.com/react/react_usestate.asp
 
-function App() {
+const images = [
+  "https://images.pexels.com/photos/8516750/pexels-photo-8516750.jpeg",
+  "https://images.pexels.com/photos/11815778/pexels-photo-11815778.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://images.pexels.com/photos/16049221/pexels-photo-16049221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  "https://images.pexels.com/photos/6956499/pexels-photo-6956499.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+]
+
+export default function App() {  //we need to export this function because we are using it in 'index.js'
+  const [current, setCurrent] = useState(0)
+
+  function nextSlide() {
+    setCurrent(current === images.length - 1 ? 0 : current + 1); //is equal to => if current is equal to length of images nextSlide = 0 else add 1 to current
+  }
+
+  function prevSlide() {
+    setCurrent(current === 0 ? images.length - 1 : current - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Carousel Project</h2>
+      <div className="slider">
+        <div className="left-arrow" onClick={prevSlide}>
+          ⬅
+        </div>
+        <div className="right-arrow" onClick={nextSlide}>
+          ⮕
+        </div>
+        {images.map(
+          (image, index) =>
+            current === index && (
+              <div key={image} className="slide">
+                <img src={image} alt="images" />
+              </div>
+            )
+        )}
+      </div>
     </div>
   );
 }
-
-export default App;
